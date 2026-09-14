b7461699ea240454f2d21a49291aea0be704e4e3
dayadc1234-lgtm <dayadc1234@gmail.com>
2026-09-14 09:49:50 -0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Replace FORM_ID with the ID from your Formspree form.
-// Formspree will deliver the submission to the email connected to that form.
-window.APP_CONFIG = {
-  formEndpoint: 'https://formspree.io/f/xoeqojka',
-  notificationEmail: 'dayadc1234@gmail.com'
-};
